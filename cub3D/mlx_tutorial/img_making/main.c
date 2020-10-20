c8fb8128c2522109dc6705ccf83ebcd2c12fa886
hminn <hmkim0008@gmail.com>
2020-10-20 20:01:26 +0900

    cub3d_mlx_tutorial

--- cub3D/mlx_tutorial/img_making/main.c
+++ cub3D/mlx_tutorial/img_making/main.c
@@ -7,4 +7,46 @@
 # define IMG_WIDTH 400
 # define IMG_HEIGHT 300
 
+typedef struct s_img
+{
+	void	*img_ptr;
+	int		*data;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+}			t_img;
 
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win;
+}			t_mlx;
+
+int		main(void)
+{
+	t_mlx	*mlx;
+	t_img	img;
+	int		count_w;
+	int		count_h;
+
+	mlx->mlx_ptr = mlx_init();
+	mlx->win = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Image Making Tutorial");
+	img.img_ptr = mlx_new_image(mlx->mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel, &img.size_line, &img.endian);
+
+	count_h = -1;
+	while (++count_h < IMG_HEIGHT)
+	{
+		count_w = -1;
+		while (++count_w < IMG_WIDTH)
+		{
+			if (count_w % 5)
+				img.data[count_h * IMG_WIDTH + count_w] = 0xFFF0FF;
+			else
+				img.data[count_h * IMG_WIDTH + count_w] = 0xFF0000;
+		}
+	}
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, img.img_ptr, 0, 0);
+	mlx_loop(mlx->mlx_ptr);
+	return (0);
+}
