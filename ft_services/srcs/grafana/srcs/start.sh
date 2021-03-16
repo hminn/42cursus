236dbf607acdcb6c3dd9184edb4a11ea34927510
hyeokim <hmkim0008@gmail.com>
2021-03-16 18:03:41 +0100

    Delete ft_services directory

--- ft_services/srcs/grafana/srcs/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-grafana-server --homepath=/usr/share/grafana
--- ft_services/srcs/mysql/srcs/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p /run/mysqld
-sleep 5
-mysql_install_db --user=root
-mysqld -u root --bootstrap < /mysql.sql
-mysqld -u root
