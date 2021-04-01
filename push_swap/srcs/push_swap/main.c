4b348405d7ee5c5177e6772cc34af165a4819252
hyeokim <hyeokim@student.42seoul.kr>
2021-04-01 17:00:53 +0900

    last norm

--- push_swap/srcs/push_swap/main.c
+++ push_swap/srcs/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: hyeokim <hyeokim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 15:59:02 by hyeokim           #+#    #+#             */
-/*   Updated: 2021/04/01 16:34:41 by hyeokim          ###   ########.fr       */
+/*   Updated: 2021/04/01 17:00:40 by hyeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int		main(int argc, char *argv[])
 		return (0);
 	init_stack(&a_stack, argc, argv);
 	init_stack(&b_stack, 0, NULL);
-
 	if (!sort_check(a_stack))
 	{
 		if (a_stack->size <= 3 || a_stack->size == 5)
